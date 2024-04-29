891ac154ccd98cc24215e590f1c5e933c2a31568
Oulaya omar <oulaya.omar@gmail.com>
2024-04-29 16:08:10 +0200

    2.1

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_nom/<str:post_str>')
+@app.route('/fiche_nom/<post_str>')
 def Readfiche(post_str):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
