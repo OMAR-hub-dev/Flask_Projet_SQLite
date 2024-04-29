4c049baf7e217da6eade1b513dc46925cceafe99
Oulaya omar <oulaya.omar@gmail.com>
2024-04-29 16:39:22 +0200

    2.4

--- __init__.py
+++ __init__.py
@@ -41,28 +41,28 @@ def authentification():
 
 @app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
-    if request.method == 'GET':
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
+@app.route('/fiche_nom/<post_str>')
+def ReadficheNom(post_str):
+      if request.method == 'GET':
         # Vérifier les identifiants
         if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
             conn = sqlite3.connect('database.db')
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+            cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_str,))
             data = cursor.fetchall()
             conn.close()
             # Rendre le template HTML et transmettre les données
             return render_template('read_data.html', data=data)
         else : 
             render_template('formulaire_authentification.html', error=False)
-
-@app.route('/fiche_nom/<post_str>')
-def ReadficheN(post_str):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_str,))
-    data = cursor.fetchall()
-    conn.close()
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
 
 @app.route('/consultation/')
 def ReadBDD():
